5004e41c961d269e4f10f388f9185d605605f6a1
misham2001 <117187747+misham2001@users.noreply.github.com>
2024-11-25 13:35:50 +0530

    Update tablelayout.jsx

--- src/components/section2/tablelayout.jsx
+++ src/components/section2/tablelayout.jsx
@@ -23,12 +23,9 @@ function Tablelayout() {
         <div className="flex items-center space-x-2">
          <input type="checkbox" className="form-checkbox" />
           <span className="flex items-center space-x-1">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-blue-500">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-blue-500 mr-2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
               </svg>
-            <span className="material-icons text-blue-500">              
-                 person
-            </span>
             {info.getValue()}
           </span>
         </div>
